fc4f48884855cf1179957be849490db716342b6a
nitroqwe2010 <nitroasd71@gmail.com>
2025-02-13 05:36:31 -0500

    setTimeout , setInterval

--- main.js
+++ main.js
@@ -1,24 +1,15 @@
 function updateClock() {
-    const clockElement = document.getElementById('clock');
-    
-    // Joriy vaqtni olish
     const now = new Date();
-    
+
     let hours = now.getHours();
     let minutes = now.getMinutes();
     let seconds = now.getSeconds();
-    
-    // Agar soat yoki daqiqa yoki soniya 10dan kichik bo'lsa, oldiga 0 qo'shish
-    hours = hours < 10 ? '0' + hours : hours;
-    minutes = minutes < 10 ? '0' + minutes : minutes;
-    seconds = seconds < 10 ? '0' + seconds : seconds;
-    
-    // Soatni ekranga chiqarish
-    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+
+    document.getElementById('hours').textContent = hours < 10 ? '0' + hours : hours;
+    document.getElementById('minutes').textContent = minutes < 10 ? '0' + minutes : minutes;
+    document.getElementById('seconds').textContent = seconds < 10 ? '0' + seconds : seconds;
 }
 
-// Har 1 soniyada soatni yangilab borish
+// Har 1 soniyada yangilash
 setInterval(updateClock, 1000);
-
-// Dastlabki birinchi vaqti ko'rsatish
 updateClock();
